65e8809981101da094bb668e0c61d7c9d44df6c4
Euro <abaoeuro2002@gmail.com>
2024-10-02 02:30:54 +0800

    server url changes to live client url

--- server/index.js
+++ server/index.js
@@ -36,7 +36,8 @@ app.use("/api/activityStats", require("./routes/activityStatsRoute"));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://watch-me.vercel.app",
     methods: ["GET", "POST"],
   },
 });
