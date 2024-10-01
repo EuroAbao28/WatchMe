2541ce9b7ba5928dc47633741644240f7f8d0cbb
Euro <abaoeuro2002@gmail.com>
2024-10-02 04:37:41 +0800

    value increment animation

--- client/src/components/ActivityStats.jsx
+++ client/src/components/ActivityStats.jsx
@@ -12,18 +12,23 @@ function ActivityStats() {
 
       <div className="flex gap-4 mt-4">
         <div className="flex flex-col items-center justify-center flex-1 py-4 text-orange-500 rounded cursor-default hover:bg-orange-500/10 bg-orange-500/5 outline outline-1 outline-orange-500/20">
-          <CountUp start={0} end={visits} duration={5} />
-
+          <p className="font-extrabold ">
+            <CountUp start={0} end={visits} duration={5} />
+          </p>
           <p className="text-xs font-semibold ">Visits</p>
         </div>
 
         <div className="flex flex-col items-center justify-center flex-1 py-4 text-green-500 rounded cursor-default hover:bg-green-500/10 bg-green-500/5 outline outline-1 outline-green-500/20">
-          <h3 className="font-extrabold ">{activeUsers}</h3>
+          <p className="font-extrabold ">
+            <CountUp start={0} end={activeUsers} duration={5} />
+          </p>
           <p className="text-xs font-semibold ">Active</p>
         </div>
 
         <div className="flex flex-col items-center justify-center flex-1 py-4 text-indigo-500 rounded cursor-default hover:bg-indigo-500/10 bg-indigo-500/5 outline outline-1 outline-indigo-500/20">
-          <h3 className="font-extrabold ">{watched}</h3>
+          <p className="font-extrabold ">
+            <CountUp start={0} end={watched} duration={5} />
+          </p>
           <p className="text-xs font-semibold ">Watched</p>
         </div>
       </div>
